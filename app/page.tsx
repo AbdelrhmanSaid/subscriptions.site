dadb07bfd35486c68278c9402de6b5f9cc34c37f
Abdelrhman Said <abdelrhmansaidzaki@hotmail.com>
2025-09-09 00:34:16 +0300

    use next

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="text-center text-2xl font-sans h-screen flex items-center justify-center">
+      Something great is coming soon...
+    </div>
+  );
+}
