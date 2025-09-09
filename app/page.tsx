bd91b8381b1fbe7f078d9cc693d2680ec4a3ca8c
Abdelrhman Said <abdelrhmansaidzaki@hotmail.com>
2025-09-09 15:15:20 +0300

    update identity

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="text-center text-2xl font-sans h-screen flex items-center justify-center">
-      Something great is coming soon...
+    <div className="flex flex-col items-center justify-center gap-4 h-screen">
+      <Image src="/logo.png" alt="logo" width={100} height={100} />
+      <p className="text-2xl font-sans">Something great is coming soon</p>
     </div>
   );
 }
